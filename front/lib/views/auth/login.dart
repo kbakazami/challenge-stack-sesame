11030a6ef91b4ef5884540906293a15748cd787d
Sofia Chaudhry <sofia.chaudhry2606@gmail.com>
2024-07-17 11:07:00 +0200

    feature/11 - Authentification google
    :add Stylized login screen
    
    :add Added application logo & google svg

--- front/lib/views/auth/login.dart
+++ front/lib/views/auth/login.dart
@@ -1,6 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:front/constants/colors.dart';
 
 class Login extends StatelessWidget {
 
@@ -11,7 +11,38 @@ class Login extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       alignment: Alignment.center,
-      child: const Text('Page Login'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Sésame", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: AppColors.primary)),
+          const SizedBox(height: 40),
+          const Image(image: AssetImage('assets/icons/logo.png'), width: 120),
+          const SizedBox(height: 40),
+          TextButton(
+              onPressed: () => print('clicked'),
+              style: TextButton.styleFrom(
+                foregroundColor: AppColors.primary,
+                padding: EdgeInsets.symmetric(vertical: 10),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                  side: BorderSide(
+                    color: AppColors.primary,
+                    width: 2
+                  )
+                )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset('assets/icons/google.svg', width: 30),
+                  const SizedBox(width: 20),
+                  const Text("Connexion via google", style: TextStyle(fontWeight: FontWeight.w600, color: AppColors.primary)),
+                ],
+              ),
+          )
+        ],
+      ),
     );
   }
 }
