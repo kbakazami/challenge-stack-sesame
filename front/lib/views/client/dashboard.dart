8be7198d3bc8ef476b5be89c6927c8ea6c783576
Sofia Chaudhry <sofia.chaudhry2606@gmail.com>
2024-07-18 15:14:53 +0200

    feature/11 - Authentification google
    :delete Removed unused import

--- front/lib/views/client/dashboard.dart
+++ front/lib/views/client/dashboard.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
-
-import '../../constants/colors.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
